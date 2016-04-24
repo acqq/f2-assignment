e82f23ffb68e224ba13053e4f9df17d854157334
Thunchanok Naosuwan <nokiiest@gmail.com>
2016-04-24 22:54:27 +0700

    Create SpaceShip.java
    This class will create a space ship with blue color

---
+++ SpaceShip.java
@@ -0,0 +1,29 @@
+package f2.spw;
+
+import java.awt.*;
+
+public class SpaceShip extends Sprite{
+
+	int step = 8;
+	
+	public SpaceShip(int x, int y, int width, int height) {
+		super(x, y, width, height);
+		
+	}
+
+	@Override
+	public void draw(Graphics2D g) {
+		g.setColor(Color.BLUE);
+		g.fillRect(x, y, width, height);
+		
+	}
+
+	public void move(int direction){
+		x += (step * direction);
+		if(x < 0)
+			x = 0;
+		if(x > 550 - width)
+			x = 550 - width;
+	}
+
+}
