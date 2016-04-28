65aab4379bcea892b785e46cb617bf07124b2408
Thunchanok Naosuwan <nokiiest@gmail.com>
2016-04-28 21:56:49 +0700

    #4 game panel setting

---
+++ GamePanel.java
@@ -0,0 +1,39 @@
+package f2.spw;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel {
+	
+	private BufferedImage bi;	
+	Graphics2D big;
+	ArrayList<Sprite> sprites = new ArrayList<Sprite>();
+
+	public GamePanel() {
+		bi = new BufferedImage(550, 800, BufferedImage.TYPE_INT_ARGB);
+		big = (Graphics2D) bi.getGraphics();
+		big.setBackground(Color.BLACK);
+	}
+
+	public void updateGameUI(GameReporter reporter){
+		big.clearRect(0, 0, 550, 800);
+		
+		big.setColor(Color.WHITE);		
+		big.drawString(String.format("%08d", reporter.getScore()), 300, 20);
+		for(Sprite s : sprites){
+			s.draw(big);
+		}
+		
+		repaint();
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.drawImage(bi, null, 0, 0);
+	}
+
+}
